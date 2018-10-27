dea9ef03e3a9d2560ea1da9e3a0de39f89fdb32d
Khai Nguyen <kqn001@bucknell.edu>
2018-10-27 16:57:32 -0700

    fixing height

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
 			else {
 				names = data.Attributes.members
 				$.each(names, function (k, v) {
-					$("#" + k + " .water").css("height", Math.min(5 + v * 30, 270));
+					$("#" + k + " .water").css("height", Math.min(20 + v * 30, 320));
 				});
 			}
 		});
@@ -73,7 +73,7 @@ $(document).ready(function () {
 				names = data.Attributes.members
 				console.log(names);
 				$.each(names, function (k, v) {
-					$("#" + k + " .water").css("height", Math.min(5 + v * 30, 270));
+					$("#" + k + " .water").css("height", Math.min(20 + v * 30, 320));
 				});
 			}
 		});
@@ -85,7 +85,7 @@ $(document).ready(function () {
 			else {
 				names = data.Item.members;
 				$.each(names, function (k, v) {
-					$("#" + k + " .water").css("height", Math.min(5 + v * 30, 270));
+					$("#" + k + " .water").css("height", Math.min(20 + v * 30, 320));
 				});
 			}
 		});
@@ -102,7 +102,7 @@ $(document).ready(function () {
 				.html("<div class = \"water\"></div> <div class = \"name\">" + input + "<h1>" + k + "<h1></div>");
 			$('body').append($newDiv)
 			$("#" + k).css("left", left);
-			$("#" + k + " .water").css("height", Math.min(5 + v * 30, 270));
+			$("#" + k + " .water").css("height", Math.min(20 + v * 30, 320));
 			left += ($('body').width() - 180 * (Object.keys(names).length + 1)) / (Object.keys(names).length - 1) + 180;
 			$("#" + k + " .name .input").keypress(function (event) {
 				var keycode = (event.keyCode ? event.keyCode : event.which);
