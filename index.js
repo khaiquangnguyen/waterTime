00bb97bfe391b8039a86c6df75729a60c4bcf8eb
KhaiNguyen <kqn001@bucknell.edu>
2018-10-30 20:31:42 -0700

    done

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let DATA_ENTRY_NAME = 'Oct22';
+let CUP_HEIGHT = 310;
+let MIN_WATER_HEIGHT = 10;
+let WATER_PER_HOUR_RELAX = 15;
+let WATER_PER_PRODUCTIVITY = 25;
+
 $(document).ready(function () {
 	// Amazon access
 	// Initialize the Amazon Cognito credentials provider
@@ -13,7 +19,7 @@ $(document).ready(function () {
 	var ddbTable = 'TEAM';
 	var item_params = {
 		Key: {
-			"name": "teamwork"
+			"name": DATA_ENTRY_NAME
 		},
 		TableName: ddbTable
 	}
@@ -21,19 +27,27 @@ $(document).ready(function () {
 	docClient.get(item_params, function (err, data) {
 		if (err) console.log(err, err.stack); // an error occurred
 		else {
-			name_list = data.Item.members;
-			draw_cups(name_list);
-			setInterval(live_update,250);
+			date = data.Item.startdate;
+			names = data.Item.productivity;
+			draw_cups(names);
+			setInterval(live_update, 500);
+			setInterval(function () {
+				start_time = moment(date).format('MMMM Do YYYY');
+				end_time = moment(date).add(14, 'days')
+				remaining_time = end_time.diff(moment(), "days")
+				$("#timestamp").html("<h2> Today is " + moment().format('MMMM Do YYYY') + ". There are " + remaining_time + " days left until we can rest and celebrate! <h2>");
+				$("#period").html("<h2>Hey guys! Our journey begins since " + start_time + ". <h2>");
+			});
 		}
 	});
 
 
-	function update_dynamo(person, value) {
-		person = "members." + person;
+	function update_dynamo(person, value, is_productivity) {
+		person = (is_productivity ? "productivity." + person : "relax." + person);
 		var params = {
 			TableName: ddbTable,
 			Key: {
-				"name": "teamwork",
+				"name": DATA_ENTRY_NAME
 			},
 			UpdateExpression: "set " + person + " = " + person + "+ :val",
 			ExpressionAttributeValues: {
@@ -42,23 +56,25 @@ $(document).ready(function () {
 			ReturnValues: "UPDATED_NEW"
 		};
 		docClient.update(params, function (err, data) {
-			$(".input").val("");
 			if (err) console.log(err);
 			else {
-				names = data.Attributes.members
-				$.each(names, function (k, v) {
-					$("#" + k + " .water").css("height", Math.min(20 + v * 25, 320));
+				$("input").val("");
+				$.each(productivity, function (k, v) {
+					$("#" + k + " .productive_water").css("height", Math.min(MIN_WATER_HEIGHT + v * WATER_PER_PRODUCTIVITY, CUP_HEIGHT));
+				});
+				$.each(relax, function (k, v) {
+					$("#" + k + " .relax_water").css("height", Math.min(MIN_WATER_HEIGHT + v * WATER_PER_HOUR_RELAX, CUP_HEIGHT));
 				});
 			}
 		});
 	}
 
-	function reset_dynamo(person) {
-		person = "members." + person;
+	function reset_dynamo(person, is_productivity) {
+		person = (is_productivity ? "productivity." + person : "relax." + person);
 		var params = {
 			TableName: ddbTable,
 			Key: {
-				"name": "teamwork",
+				"name": DATA_ENTRY_NAME,
 			},
 			UpdateExpression: "set " + person + " = :r",
 			ExpressionAttributeValues: {
@@ -67,25 +83,31 @@ $(document).ready(function () {
 			ReturnValues: "UPDATED_NEW"
 		};
 		docClient.update(params, function (err, data) {
-			$(".input").val("");
 			if (err) console.log(err);
 			else {
-				names = data.Attributes.members
-				console.log(names);
-				$.each(names, function (k, v) {
-					$("#" + k + " .water").css("height", Math.min(20 + v * 25, 320));
+				$("input").val("");
+				$.each(productivity, function (k, v) {
+					$("#" + k + " .productive_water").css("height", Math.min(MIN_WATER_HEIGHT + v * WATER_PER_PRODUCTIVITY, CUP_HEIGHT));
+				});
+				$.each(relax, function (k, v) {
+					$("#" + k + " .relax_water").css("height", Math.min(MIN_WATER_HEIGHT + v * WATER_PER_PRODUCTIVITY, CUP_HEIGHT));
 				});
 			}
 		});
 	}
 
+
 	function live_update() {
 		docClient.get(item_params, function (err, data) {
 			if (err) console.log(err, err.stack); // an error occurred
 			else {
-				names = data.Item.members;
-				$.each(names, function (k, v) {
-					$("#" + k + " .water").css("height", Math.min(20 + v * 25, 320));
+				productivity = data.Item.productivity;
+				relax = data.Item.relax;
+				$.each(productivity, function (k, v) {
+					$("#" + k + " .productive_water").css("height", Math.min(MIN_WATER_HEIGHT + v * WATER_PER_PRODUCTIVITY, CUP_HEIGHT));
+				});
+				$.each(relax, function (k, v) {
+					$("#" + k + " .relax_water").css("height", Math.min(MIN_WATER_HEIGHT + v * WATER_PER_HOUR_RELAX, CUP_HEIGHT));
 				});
 			}
 		});
@@ -94,24 +116,39 @@ $(document).ready(function () {
 	function draw_cups(names) {
 		left = 90;
 		$.each(names, function (k, v) {
-			var input = "<input class='input' id = '" + k + "' type='number' placeholder='work hour'>"
-
+			var input = "<input class='productive_input' id = '" + k + "' type='number' placeholder='productive hrs'> <input class='relax_input' id = '" + k + "' type='number' placeholder='happy hrs'>"
 			var $newDiv = $("<div/>") // creates a div element
 				.attr("id", k) // adds the id
-				.addClass("cup") // add a class
-				.html("<div class = \"water\"></div> <div class = \"name\">" + input + "<h1>" + k + "<h1></div>");
+				.addClass("cup_set") // add a class
+				.html("<div class = 'productive_cup'> <div class = 'productive_water'></div> </div> <div class = 'relax_cup'> <div class = \"relax_water\"></div> </div>" + input + "<div class = \"name\">" + "<h3>" + k + "<h3></div>");
 			$('body').append($newDiv)
 			$("#" + k).css("left", left);
-			$("#" + k + " .water").css("height", Math.min(20 + v * 25, 320));
-			left += ($('body').width() - 180 * (Object.keys(names).length + 1)) / (Object.keys(names).length - 1) + 180;
-			$("#" + k + " .name .input").keypress(function (event) {
+			left += ($('body').width() - 300 * (Object.keys(names).length + 1)) / (Object.keys(names).length - 1) + 300;
+			$("#" + k + " .productive_input").keypress(function (event) {
 				var keycode = (event.keyCode ? event.keyCode : event.which);
 				if (keycode == '13') {
 					value = parseInt($(this).val());
 					if (value === 0) {
-						reset_dynamo($(this).attr("id"));
+						reset_dynamo($(this).attr("id"), true);
 					} else {
-						update_dynamo($(this).attr("id"), value);
+						update_dynamo($(this).attr("id"), value, true);
+					}
+				}
+				// Stop the event from propogation to other handlers
+				// If this line will be removed, then keypress event handler attached
+				// at document level will also be triggered
+				event.stopPropagation();
+				return;
+			});
+			$("#" + k + " .relax_input").keypress(function (event) {
+				console.log('a');
+				var keycode = (event.keyCode ? event.keyCode : event.which);
+				if (keycode == '13') {
+					value = parseInt($(this).val());
+					if (value === 0) {
+						reset_dynamo($(this).attr("id"), false);
+					} else {
+						update_dynamo($(this).attr("id"), value, false);
 					}
 				}
 				//Stop the event from propogation to other handlers
@@ -123,3 +160,4 @@ $(document).ready(function () {
 		});
 	}
 });
+
